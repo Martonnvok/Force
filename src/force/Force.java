b3ecf32727a047c1eea74362538404808290d0b1
Martonnvok <martonsom@gmail.com>
2023-09-25 15:30:44 +0200

    hibák eltüntetése

--- src/force/Force.java
+++ src/force/Force.java
@@ -4,36 +4,36 @@ public class Force {
 
     public static void main(String[] args) {
         for (int i = 0; i < 10; i++) {
-            hiba hiba hiba hiba
+
             System.out.println("yolo");
         }
         for (int i = 0; i < 10; i++) {
-            hiba hiba hiba hiba
+
             System.out.println("yolo");
         }
         for (int i = 0; i < 10; i++) {
-            hiba hiba hiba hiba
+
             System.out.println("yolo");
         }
         for (int i = 0; i < 10; i++) {
-            hiba hiba hiba hiba
+
             System.out.println("yolo");
         }
         
         for (int i = 0; i < 10; i++) {
-            hiba hiba hiba hiba
+
             System.out.println("yolo");
         }
         for (int i = 0; i < 10; i++) {
-            hiba hiba hiba hiba
+
             System.out.println("yolo");
         }
         for (int i = 0; i < 10; i++) {
-            hiba hiba hiba hiba
+
             System.out.println("yolo");
         }
         for (int i = 0; i < 10; i++) {
-            hiba hiba hiba hiba
+
             System.out.println("yolo");
         }
 
