c5817c56e340b8b7691e4d14ef8eb02193d20d68
Martonnvok <115223813+Martonnvok@users.noreply.github.com>
2023-09-25 15:14:40 +0200

    force hiba

--- src/force/Force.java
+++ src/force/Force.java
@@ -7,7 +7,7 @@ public class Force {
     
     public static void main(String[] args) {
         for (int i = 0; i < 10; i++) {
-            
+            hiba hiba hiba hiba
         }
     }
     
